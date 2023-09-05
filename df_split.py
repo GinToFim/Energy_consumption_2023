6141ec451c1a434534c905c141ba0adae6ba61b6
GinToFim <shlf6673@gmail.com>
2023-09-05 18:11:29 +0900

    Upload train valid split

---
+++ df_split.py
@@ -0,0 +1,13 @@
+import argparse
+import pandas as pd
+import numpy as np
+import os
+from omegaconf import OmegaConf
+
+
+def train_valid_split():
+    pass
+
+if __name__ == "__main__":
+    # parser 객체 생성
+    parser = argparse.ArgumentParser()
